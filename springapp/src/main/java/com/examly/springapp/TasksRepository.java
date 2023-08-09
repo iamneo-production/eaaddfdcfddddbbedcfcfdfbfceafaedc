8742f555881281ac8f473385fd241492485aec07
Iamneo <techadmin@iamneo.ai>
2023-08-09 10:17:02 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/TasksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.examly.springapp.model.Tasks;
-
-public interface TasksRepository extends CrudRepository<Tasks, Integer>
-{ 
-    
-}
